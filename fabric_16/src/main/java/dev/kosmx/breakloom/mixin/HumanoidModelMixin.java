88970a625bbb9df2f09102d22dd7496653b5df34
KosmX <kosmx.mc@gmail.com>
2022-04-20 14:01:14 +0200

    Now with YARN mappings!

--- fabric_16/src/main/java/dev/kosmx/breakloom/mixin/HumanoidModelMixin.java
+++ fabric_16/src/main/java/dev/kosmx/breakloom/mixin/HumanoidModelMixin.java
@@ -1,20 +1,20 @@
 package dev.kosmx.breakloom.mixin;
 
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(HumanoidModel.class)
+@Mixin(BipedEntityModel.class)
 public class HumanoidModelMixin {
     @Shadow public ModelPart leftArm;
 
-    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At("TAIL"))
+    @Inject(method = "setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V", at = @At("TAIL"))
     private void func(LivingEntity entity, float f, float g, float h, float i, float j, CallbackInfo ci) {
-        this.leftArm.xRot += Math.PI;
+        this.leftArm.pitch += Math.PI;
     }
 }
--- fabric_18/src/main/java/dev/kosmx/breakloom/mixin/HumanoidModelMixin.java
+++ fabric_18/src/main/java/dev/kosmx/breakloom/mixin/HumanoidModelMixin.java
@@ -1,8 +1,8 @@
 package dev.kosmx.breakloom.mixin;
 
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,13 +10,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(HumanoidModel.class)
+@Mixin(BipedEntityModel.class)
 public class HumanoidModelMixin {
     @Final
     @Shadow public ModelPart leftArm;
 
-    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/Entity;FFFFF)V", at = @At("TAIL"))
-    private void func(Entity entity, float f, float g, float h, float i, float j, CallbackInfo ci) {
-        this.leftArm.xRot += Math.PI;
+    @Inject(method = "setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V", at = @At("TAIL"))
+    private void func(LivingEntity entity, float f, float g, float h, float i, float j, CallbackInfo ci) {
+        this.leftArm.pitch += Math.PI;
     }
 }
